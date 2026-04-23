eecd1530eeec08b67a81c3fbbe7207e64baf41cb
Harjot1807 <harjotsingh180708@gmail.com>
2026-04-23 13:03:44 -0600

    started working on my chicken background- mainmnu

--- sketch.js
+++ sketch.js
@@ -5,21 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let mMenuBg; 
 let state = "mainMenu";
+
+function preload(){
+  mMenuBg = loadImage("chicken.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
   displayMainMenu();
 }
 
 //functions that displays the main menu at the start of the game
 function displayMainMenu() {
   if (state === "mainMenu"){
+    background(mMenuBg);
     fill('white');
     rect(width/4, height/4, width/2, height/6);
     rect(width/4, height/2, width/2, height/6);
@@ -27,6 +31,6 @@ function displayMainMenu() {
     textAlign(CENTER, CENTER);
     textSize((width+height)/20);
     text("Play", width/2, height*8/24);
-    text("Controls", width/2, height*14/24)
+    text("Controls", width/2, height*14/24);
   }
 }
